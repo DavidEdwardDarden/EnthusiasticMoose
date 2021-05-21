95a37ed21ab7ba52301bd1daa6f1145af4bd0795
DavidEdwardDarden <davidedarden83@gmail.com>
2021-05-21 13:51:48 -0500

    phase 5 complete

--- Program.cs
+++ Program.cs
@@ -40,4 +40,26 @@ void MooseSays(string message)
     ");
 }
 
-// MooseSays();
+bool MooseAsks(string question)
+{
+    Console.Write($"{question} (Y/N): ");
+    string answer = Console.ReadLine().ToLower();
+
+    while (answer != "y" && answer != "n")
+    {
+        Console.Write($"{question} (Y/N): ");
+        answer = Console.ReadLine().ToLower();
+    }
+
+    if (answer == "y")
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+bool isTrue = MooseAsks("Is Canada real?");
+Console.WriteLine(isTrue);
